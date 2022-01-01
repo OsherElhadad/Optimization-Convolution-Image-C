ba041506ebb14fb2f4e6ef5e6b1177849e289eea
osher <oshereli2@walla.com>
2022-01-01 20:31:16 +0200

    only myfunction optimized

--- showBMP.c
+++ showBMP.c
@@ -36,33 +36,33 @@ char flag; // chooses which kernel to execute
 
 void getImage(char* filename) {
 
-	// allocate space for image data structure
-	image = (Image *) malloc(sizeof(Image));
-	if (image == NULL) {
-		printf("Error allocating space for image");
-		exit(0);
-	}
+    // allocate space for image data structure
+    image = (Image *) malloc(sizeof(Image));
+    if (image == NULL) {
+        printf("Error allocating space for image");
+        exit(0);
+    }
 
-	if (!ImageLoad(filename, image)) {
-		exit(1);
-	}
+    if (!ImageLoad(filename, image)) {
+        exit(1);
+    }
 }
 
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glRasterPos2i(0, 0);
-	//printf("n=%ld,m=%ld\n",n,m);
-	glDrawPixels(n, m, GL_RGB, GL_UNSIGNED_BYTE, image->data);
-	glFlush();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glRasterPos2i(0, 0);
+    //printf("n=%ld,m=%ld\n",n,m);
+    glDrawPixels(n, m, GL_RGB, GL_UNSIGNED_BYTE, image->data);
+    glFlush();
 }
 
 void myreshape(int h, int w) {
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0.0, (GLfloat) n, 0.0, (GLfloat) m);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glViewport(0, 0, h, w);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, (GLfloat) n, 0.0, (GLfloat) m);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glViewport(0, 0, h, w);
 }
 
 #include "myfunction.c"
@@ -73,11 +73,11 @@ char* filteredSharpRsltImgName = "Filtered_Sharpen.bmp";
 
 void optimize(Image* image, char flag) {
 
-	struct timeval startTime;
-	struct timeval endTime;
-	struct rusage ru;
-	getrusage(RUSAGE_SELF, &ru); // start timer
-	startTime = ru.ru_utime;
+    struct timeval startTime;
+    struct timeval endTime;
+    struct rusage ru;
+    getrusage(RUSAGE_SELF, &ru); // start timer
+    startTime = ru.ru_utime;
 
     int num = 10;
     for (int i = 0; i < num; ++i) {
@@ -85,59 +85,59 @@ void optimize(Image* image, char flag) {
     }
     //myfunction(image, picName, blurRsltImgName, sharpRsltImgName, filteredBlurRsltImgName, filteredSharpRsltImgName, flag);
 
-	getrusage(RUSAGE_SELF, &ru); // end timer
-	endTime = ru.ru_utime;
-	double tS = startTime.tv_sec * 1000000.0 + (startTime.tv_usec);
-	double tE = endTime.tv_sec * 1000000.0 + (endTime.tv_usec);
-	printf("Total runtime: %f ms\n", (tE - tS) / 1000.0 / num);
+    getrusage(RUSAGE_SELF, &ru); // end timer
+    endTime = ru.ru_utime;
+    double tS = startTime.tv_sec * 1000000.0 + (startTime.tv_usec);
+    double tE = endTime.tv_sec * 1000000.0 + (endTime.tv_usec);
+    printf("Total runtime: %f ms\n", (tE - tS) / 1000.0 / num);
 }
 
 static void key(unsigned char c, int x, int y) {
-	switch (c) {
-	case 27:
-		exit(0);
-		break;
-	}
+    switch (c) {
+        case 27:
+            exit(0);
+            break;
+    }
 }
 
 int main(int argc, char **argv) {
 
-	getImage(argv[1]);
-	n = image->sizeX; // width
-	m = image->sizeY; // height
-	char buffer[256];
-	if (argc != 3) {
-		printf("please specify a kernel option\n");
-		return 0;
-	}
-	flag = argv[2][0];
-	if (flag == '1') {
-		printf("kernel number 1 was chosen\n");
-	}
-	else {
-		printf("kernel number 2 was chosen\n");
-	}
-	
-	
+    getImage(argv[1]);
+    n = image->sizeX; // width
+    m = image->sizeY; // height
+    char buffer[256];
+    if (argc != 3) {
+        printf("please specify a kernel option\n");
+        return 0;
+    }
+    flag = argv[2][0];
+    if (flag == '1') {
+        printf("kernel number 1 was chosen\n");
+    }
+    else {
+        printf("kernel number 2 was chosen\n");
+    }
 
-	// get pic name
-	strcpy(picName, argv[1]);
-	picName[strlen(argv[1])] = 0;
 
-	// build window title
-	sprintf(buffer, "%s of %s", WINDOW_TITLE, picName);
 
-	optimize(image, flag);
+    // get pic name
+    strcpy(picName, argv[1]);
+    picName[strlen(argv[1])] = 0;
 
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(n, m);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow(buffer);
-	glutReshapeFunc(myreshape);
-	glutKeyboardFunc(key);
-	glutDisplayFunc(display);
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glutMainLoop();
-	return (0);
+    // build window title
+    sprintf(buffer, "%s of %s", WINDOW_TITLE, picName);
+
+    optimize(image, flag);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(n, m);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow(buffer);
+    glutReshapeFunc(myreshape);
+    glutKeyboardFunc(key);
+    glutDisplayFunc(display);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glutMainLoop();
+    return (0);
 }
