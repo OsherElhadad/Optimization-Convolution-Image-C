1b7d1b66cebb25c69d7d8de4fcc0e60f969cb6b9
osher <oshereli2@walla.com>
2022-01-02 01:25:18 +0200

    created smooth2

--- myfunction.c
+++ myfunction.c
@@ -1,4 +1,6 @@
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct {
    unsigned char red;
@@ -264,13 +266,371 @@ void smooth(pixel *src, pixel *dst, int kernel[3], int kernelScale, bool filter)
 	}
 }
 
+
+
+/*
+* Apply the kernel over each pixel.
+* Ignore pixels where the kernel exceeds bounds. These are pixels with row index smaller than kernelSize/2 and/or
+* column index smaller than kernelSize/2
+*/
+void smooth2(Image *image1, int weight1, int weight2, int kernelScale, bool filter) {
+
+    char *dest = (char *)malloc(3*m*n), *dest1 = dest, *dest2 = dest;
+    char *data = image1->data, *src1 = data;
+    int i, j, m3 = 3*m;
+
+    //*******************************************************************
+    // optimization- calculate (dim - kernelSize / 2) before the loop
+    //*******************************************************************
+    int until = m - 1;
+
+    //*******************************************************************
+    // optimization- calculate pointer add 1 without calculate index
+    //*******************************************************************
+    for (int k = 0; k < m3; ++k) {
+        (*dest) = (*data);
+        data++;
+        dest++;
+    }
+    for (i = 1; i < until; i++) {
+        (*dest) = (*data);
+        data++;
+        dest++;
+        (*dest) = (*data);
+        data++;
+        dest++;
+        (*dest) = (*data);
+        data++;
+        dest++;
+        for (j = 1; j < until; j++) {
+
+            int min_intensity = 766; // arbitrary value that is higher than maximum possible intensity, which is 255*3=765
+            int max_intensity = -1; // arbitrary value that is lower than minimum possible intensity, which is 0
+            int min_row, min_col, max_row, max_col;
+            char *dataBefore = data - m3, *dataAfter = data + m3;
+            //pixel loop_pixel;
+
+            //initialize_pixel_sum(&sum);
+
+            //*******************************************************************
+            // optimization- reduce using struct pixel_sum
+            //*******************************************************************
+            int red = 0, green = 0, blue = 0;
+
+            //*******************************************************************
+            // optimization- reduce multiple loops
+            //*******************************************************************
+            if (!filter) {
+
+                // apply kernel on pixel at [ii,jj]
+                //sum_pixels_by_weight(&sum, src[calcIndex(ii, jj, dim)], kernel[kRow][kCol]);
+
+                //*******************************************************************
+                // optimization- reduce call to calcIndex
+                //*******************************************************************
+
+                //*******************************************************************
+                // optimization- reduce call to sum_pixel_by_weight
+                //*******************************************************************
+
+                red += ((unsigned char)*(dataBefore - 3)) * weight1;
+                green += ((unsigned char)*(dataBefore - 2)) * weight1;
+                blue += ((unsigned char)*(dataBefore - 1)) * weight1;
+
+                red += ((unsigned char)*dataBefore) * weight1;
+                green += ((unsigned char)*(dataBefore + 1)) * weight1;
+                blue += ((unsigned char)*(dataBefore + 2)) * weight1;
+
+                red += ((unsigned char)*(dataBefore + 3)) * weight1;
+                green += ((unsigned char)*(dataBefore + 4)) * weight1;
+                blue += ((unsigned char)*(dataBefore + 5)) * weight1;
+
+
+
+                red += ((unsigned char)*(data - 3)) * weight1;
+                green += ((unsigned char)*(data - 2)) * weight1;
+                blue += ((unsigned char)*(data - 1)) * weight1;
+
+                red += ((unsigned char)*data) * weight2;
+                green += ((unsigned char)*(data + 1)) * weight2;
+                blue += ((unsigned char)*(data + 2)) * weight2;
+
+                red += ((unsigned char)*(data + 3)) * weight1;
+                green += ((unsigned char)*(data + 4)) * weight1;
+                blue += ((unsigned char)*(data + 5)) * weight1;
+
+
+
+                red += ((unsigned char)*(dataAfter - 3)) * weight1;
+                green += ((unsigned char)*(dataAfter - 2)) * weight1;
+                blue += ((unsigned char)*(dataAfter - 1)) * weight1;
+
+                red += ((unsigned char)*dataAfter) * weight1;
+                green += ((unsigned char)*(dataAfter + 1)) * weight1;
+                blue += ((unsigned char)*(dataAfter + 2)) * weight1;
+
+                red += ((unsigned char)*(dataAfter + 3)) * weight1;
+                green += ((unsigned char)*(dataAfter + 4)) * weight1;
+                blue += ((unsigned char)*(dataAfter + 5)) * weight1;
+            } else {
+
+                // apply kernel on pixel at [ii,jj]
+                // sum_pixels_by_weight(&sum, src[calcIndex(ii, jj, dim)], kernel[kRow][kCol]);
+
+                //*******************************************************************
+                // optimization- reduce call to calcIndex
+                //*******************************************************************
+
+                //*******************************************************************
+                // optimization- reduce call to sum_pixel_by_weight
+                //*******************************************************************
+                int r, g, b, sums;
+
+                r = ((unsigned char)*(dataBefore - 3)) * weight1;
+                red += r;
+                g = ((unsigned char)*(dataBefore - 2)) * weight1;
+                green += g;
+                b = ((unsigned char)*(dataBefore - 1)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                int maxR = r, maxG = g, maxB = b, minR = r, minG = g, minB = b;
+                max_intensity = sums;
+                min_intensity = sums;
+
+                r = ((unsigned char)*dataBefore) * weight1;
+                red += r;
+                g = ((unsigned char)*(dataBefore + 1)) * weight1;
+                green += g;
+                b = ((unsigned char)*(dataBefore + 2)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+                r = ((unsigned char)*(dataBefore + 3)) * weight1;
+                red += r;
+                g = ((unsigned char)*(dataBefore + 4)) * weight1;
+                green += g;
+                b = ((unsigned char)*(dataBefore + 5)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+
+
+                r = ((unsigned char)*(data - 3)) * weight1;
+                red += r;
+                g = ((unsigned char)*(data - 2)) * weight1;
+                green += g;
+                b = ((unsigned char)*(data - 1)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+                r = ((unsigned char)*data) * weight2;
+                red += r;
+                g = ((unsigned char)*(data + 1)) * weight2;
+                green += g;
+                b = ((unsigned char)*(data + 2)) * weight2;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+                r = ((unsigned char)*(data + 3)) * weight1;
+                red += r;
+                g = ((unsigned char)*(data + 4)) * weight1;
+                green += g;
+                b = ((unsigned char)*(data + 5)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+
+
+                r = ((unsigned char)*(dataAfter - 3)) * weight1;
+                red += r;
+                g = ((unsigned char)*(dataAfter - 2)) * weight1;
+                green += g;
+                b = ((unsigned char)*(dataAfter - 1)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+                r = ((unsigned char)*dataAfter) * weight1;
+                red += r;
+                g = ((unsigned char)*(dataAfter + 1)) * weight1;
+                green += g;
+                b = ((unsigned char)*(dataAfter + 2)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+                r = ((unsigned char)*(dataAfter + 3)) * weight1;
+                red += r;
+                g = ((unsigned char)*(dataAfter + 4)) * weight1;
+                green += g;
+                b = ((unsigned char)*(dataAfter + 5)) * weight1;
+                blue += b;
+
+                sums = r + g + b;
+                if (sums <= min_intensity) {
+                    min_intensity = sums;
+                    minR = r;
+                    minG = g;
+                    minB = b;
+                } else if (sums > max_intensity) {
+                    max_intensity = sums;
+                    maxR = r;
+                    maxG = g;
+                    maxB = b;
+                }
+
+
+                red -= minR;
+                red -= maxR;
+                green -= minG;
+                green -= maxG;
+                blue -= minB;
+                blue -= maxB;
+            }
+
+            //*******************************************************************
+            // optimization- reduce call to assign_sum_to_pixel on the stack
+            //*******************************************************************
+            // assign kernel's result to pixel at [i,j]
+
+            // divide by kernel's weight
+            red = red / kernelScale;
+            green = green / kernelScale;
+            blue = blue / kernelScale;
+
+            //*******************************************************************
+            // optimization- reduce call to max and min on the stack
+            //*******************************************************************
+            // truncate each pixel's color values to match the range [0,255]
+            int maxi = (red > 0 ? red : 0);
+            (*dest) = (unsigned char) (maxi < 255 ? maxi : 255);
+            maxi = (green > 0 ? green : 0);
+            (*(dest + 1)) = (unsigned char) (maxi < 255 ? maxi : 255);
+            maxi = (blue > 0 ? blue : 0);
+            (*(dest + 2)) = (unsigned char) (maxi < 255 ? maxi : 255);
+
+
+            data += 3;
+            dest += 3;
+        }
+
+        (*dest) = (*data);
+        data++;
+        dest++;
+        (*dest) = (*data);
+        data++;
+        dest++;
+        (*dest) = (*data);
+        data++;
+        dest++;
+    }
+    for (int k = 0; k < m3; ++k) {
+        (*dest) = (*data);
+        data++;
+        dest++;
+    }
+    unsigned long mn3 = m3 * n;
+    for (int k = 0; k < mn3; ++k) {
+        (*src1) = (*dest1);
+        src1++;
+        dest1++;
+    }
+    free(dest2);
+}
+
+
+
+
 void charsToPixels(Image *charsImg, pixel* pixels) {
 
     //*******************************************************************
     // optimization- calculate pointer add 1 without calculate index
     //*******************************************************************
     unsigned long nm = n*m;
-    char *data = image->data;
+    char *data = charsImg->data;
     for (int i = 0; i < nm ; i++) {
         (*pixels).red = (*data);
         data++;
@@ -291,7 +651,7 @@ void charsToPixels2(Image *charsImg, pixel* pixels, pixel* pixels2) {
     // optimization- calculate pointer add 1 without calculate index
     //*******************************************************************
     unsigned long nm = n*m;
-    char *data = image->data;
+    char *data = charsImg->data;
     unsigned char red, green, blue;
     for (int i = 0; i < nm ; i++) {
         red = (*data);
@@ -317,7 +677,7 @@ void pixelsToChars(pixel* pixels, Image *charsImg) {
     // optimization- calculate pointer add 1 without calculate index
     //*******************************************************************
     unsigned long nm = n*m;
-    char *data = image->data;
+    char *data = charsImg->data;
     for (int i = 0; i < nm ; i++) {
         *data = pixels->red;
         data++;
@@ -377,20 +737,21 @@ void pixelsToChars(pixel* pixels, Image *charsImg) {
 //	}
 //}
 
+
 void doConvolution(Image *image, int kernel[3], int kernelScale, bool filter) {
 
-	pixel* pixelsImg = malloc(m*n*sizeof(pixel));
-	pixel* backupOrg = malloc(m*n*sizeof(pixel));
+    pixel* pixelsImg = malloc(m*n*sizeof(pixel));
+    pixel* backupOrg = malloc(m*n*sizeof(pixel));
 
-	charsToPixels2(image, pixelsImg, backupOrg);
-	//copyPixels(pixelsImg, backupOrg);
+    charsToPixels2(image, pixelsImg, backupOrg);
+    //copyPixels(pixelsImg, backupOrg);
 
-	smooth(backupOrg, pixelsImg, kernel, kernelScale, filter);
+    smooth(backupOrg, pixelsImg, kernel, kernelScale, filter);
 
-	pixelsToChars(pixelsImg, image);
+    pixelsToChars(pixelsImg, image);
 
-	free(pixelsImg);
-	free(backupOrg);
+    free(pixelsImg);
+    free(backupOrg);
 }
 
 void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sharpRsltImgName, char* filteredBlurRsltImgName, char* filteredSharpRsltImgName, char flag) {
@@ -414,25 +775,29 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
 
 	if (flag == '1') {	
 		// blur image
-		doConvolution(image, blurKernel, 9, false);
+		//doConvolution(image, blurKernel, 9, false);
+        smooth2(image, 1, 1, 9, false);
 
 		// write result image to file
 		writeBMP(image, srcImgpName, blurRsltImgName);	
 
 		// sharpen the resulting image
-		doConvolution(image, sharpKernel, 1, false);
+		//doConvolution(image, sharpKernel, 1, false);
+        smooth2(image, -1, 9, 1, false);
 		
 		// write result image to file
 		writeBMP(image, srcImgpName, sharpRsltImgName);	
 	} else {
 		// apply extermum filtered kernel to blur image
-		doConvolution(image, blurKernel, 7, true);
+		//doConvolution(image, blurKernel, 7, true);
+        smooth2(image, 1, 1, 7, true);
 
 		// write result image to file
 		writeBMP(image, srcImgpName, filteredBlurRsltImgName);
 
 		// sharpen the resulting image
-		doConvolution(image, sharpKernel, 1, false);
+		//doConvolution(image, sharpKernel, 1, false);
+        smooth2(image, -1, 9, 1, false);
 
 		// write result image to file
 		writeBMP(image, srcImgpName, filteredSharpRsltImgName);	
